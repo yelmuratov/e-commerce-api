c258d82f5434750d501162b5c164fff9d16c0c11
yelmuratov <yelimuratovsalimbay@gmail.com>
2024-12-02 10:27:40 +0500

    Crud operations done

---
+++ ECommerceAPI/Data/ECommerceDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ECommerceAPI.Models; // Assuming Product is in this namespace
+
+namespace ECommerceAPI.Data
+{
+public class ECommerceDbContext : DbContext
+{
+public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) :
+base(options) { }
+public DbSet<Product> Products { get; set; }
+}
+}
